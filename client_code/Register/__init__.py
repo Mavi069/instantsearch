9cecd003094a4c88b1fd391ff2a25990f12929bf
mac135699@gmail.com <mac135699@gmail.com>
2024-03-02 14:31:22 +0000

    Edited Register and ServerModule1

--- client_code/Register/__init__.py
+++ client_code/Register/__init__.py
@@ -4,7 +4,6 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import face_recognition
 
 class Register(RegisterTemplate):
   def __init__(self, **properties):
@@ -12,10 +11,6 @@ class Register(RegisterTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-  def load_and_encode(self, propic):
-    encodings = face_recognition.load_image_file(propic)
-    return face_recognition.face_encodings(encodings)
-    anvil.server.call('submit', encodings=encodings)
   
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
